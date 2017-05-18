5ed4b35e61ce634756450646893836d5bd157699
leead4 <leead4@gmail.com>
2017-05-18 14:06:11 -0500

    added logic for error message in views and create.html

--- website/views/view_sell_product_form.py
+++ website/views/view_sell_product_form.py
@@ -4,13 +4,17 @@ from website.models.model_product import Product
 
 def sell_product(request):
     if request.method == 'GET':
-        product_form = ProductForm()
         template_name = 'product/create.html'
+        product_form = ProductForm()
         return render(request, template_name, {'product_form': product_form})
 
     elif request.method == 'POST':
         form_data = request.POST
-
+        error_message = 1
+        if error_message == 1:
+            product_form = ProductForm()
+            template_name = 'product/create.html'
+            return render(request, template_name, {'product_form': product_form, 'error_message': 'panda'})
         p = Product(
             seller = request.user,
             title = form_data['title'],
@@ -20,4 +24,5 @@ def sell_product(request):
         )
         p.save()
         template_name = 'product/success.html'
-        return render(request, template_name, {})
+
+    return render(request, template_name, {})
