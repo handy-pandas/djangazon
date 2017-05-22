5a285f75ae93a5b4dd7a81e9ad688a04fe45c0a1
Talbot <talbotlawrence75@gmail.com>
2017-05-22 16:04:43 -0500

    this sucks

--- website/models/models.py
+++ website/models/models.py
@@ -47,6 +47,7 @@ class Product(models.Model):
     )
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    # price = models.DecimalField(max_digits=9, decimal_places=2)
     price = models.IntegerField()
     quantity = models.IntegerField()
     category = models.ForeignKey(
