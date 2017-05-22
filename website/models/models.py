81eff64d6b2f801df0c78134758c0353ced0209f
Talbot <talbotlawrence75@gmail.com>
2017-05-22 16:10:43 -0500

    changed the price field in product table to a decimal data type

--- website/models/models.py
+++ website/models/models.py
@@ -62,8 +62,8 @@ class Product(models.Model):
     )
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    # price = models.DecimalField(max_digits=9, decimal_places=2)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    # price = models.IntegerField()
     quantity = models.IntegerField()
     category = models.ForeignKey(
         Category,
