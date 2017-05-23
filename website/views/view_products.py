5efa6e907871b8e896003886dc36e23ea32dae75
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-05-23 13:12:36 -0500

    completed products in category view.

--- website/views/view_products.py
+++ website/views/view_products.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from website.models.models import Product
 
 def list_category_products(request, category_id):
-    # chosen_product = Product.objects.get(pk=product_id)
-    all_products_in_cat = Product.objects.all(pk=category_id)
+    """
+    Display all of the products for a given category.
+
+    Arguments:
+        request (GET): Get data from Product table,
+        category_id(foreign key): The Id of the selected category.
+
+    Returns: render, Combines template_name with the given dictionary and passes HttpResponse object with that rendered text.
+
+    Author:
+        wocaldwell
+    """
+    all_products_in_cat = Product.objects.filter(category_id=category_id)
+    category_name = all_products_in_cat[0].category
+    print(category_name)
     template_name = 'categoryproducts.html'
-    return render(request, template_name, {'products': all_products_in_cat})
+    return render(request, template_name, {'products': all_products_in_cat, 'name': category_name})
