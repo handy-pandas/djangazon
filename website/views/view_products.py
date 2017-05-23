e0dde29c98b9bef05999b44f7fcc8f92110806a8
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-05-23 11:39:21 -0500

    still working

--- website/views/view_products.py
+++ website/views/view_products.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from website.models.models import Product
 
-def list_category_products(request):
-    all_products_in_cat = Product.objects.all()
+def list_category_products(request, category_id):
+    # chosen_product = Product.objects.get(pk=product_id)
+    all_products_in_cat = Product.objects.all(pk=category_id)
     template_name = 'categoryproducts.html'
     return render(request, template_name, {'products': all_products_in_cat})
