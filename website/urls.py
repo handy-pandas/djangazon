204bf1bbe886cd3e4747cac5ca41e3a4a34177e5
Adam Myers <ANMyers53@yahoo.com>
2017-05-23 09:47:20 -0500

    Functional as far as i know

--- website/urls.py
+++ website/urls.py
@@ -5,11 +5,8 @@ from django.conf.urls import url
 from website.views.views import *
 from website.views.product_details_view import *
 from website.views.view_sell_product_form import sell_product
-<<<<<<< HEAD
 from website.views.view_category_products import category_products
-=======
 from website.views.add_payment_to_profile import add_payment
->>>>>>> 5478f4cd0ab50d496a77cc939310f6867a5bdfc3
 
 
 app_name = "website"
@@ -20,7 +17,7 @@ urlpatterns = [
     url(r'^register$', register, name='register'),
     url(r'^sell$', sell_product, name='sell'),
     url(r'^products$', list_products, name='list_products'),
-    url(r'^categories$', category_products, name='categories')
+    url(r'^categories$', category_products, name='categories'),
     url(r'^product_details/(?P<product_id>.+?)/$', product_details, name='product_details'),
     url(r'^profile$', profile, name='profile'),
     url(r'^add_payment$', add_payment, name='add_payment')
