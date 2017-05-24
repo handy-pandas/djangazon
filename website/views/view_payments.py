133e14ed2674c9345e4f2740e4a4c697e23c7065
Adam Myers <ANMyers53@yahoo.com>
2017-05-24 13:52:42 -0500

    Only part missing is adding products to an order

--- website/views/view_payments.py
+++ website/views/view_payments.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from website.models.models import Order, Product, Payment
 
 def view_payments(request):
-    """This function allows the after the confirmation of an order to choose a payment type.
+    """This function allows the user after the confirmation of an order to choose a payment type.
     
     Arguments:
         request (List): A list of tuples from the database pertaining to payment
@@ -18,20 +18,10 @@ def view_payments(request):
     """
     form_data = request.POST
 
-    if form_data['payment_id'] == 'Needed':
-        payments = Payment.objects.filter(user=request.user)
+    payments = Payment.objects.filter(user=request.user)
 
-        context = { 'payments': payments, 'order_id': form_data['order_id'] }
+    context = { 'payments': payments, 'order_id': form_data['order_id'] }
+    template_name = 'choose_payment.html'
+    return render(request, template_name, context)
 
-        template_name = 'choose_payment.html'
-        return render(request, template_name, context)
-
-    else:
-
-        # order_id is form_data['order_id']
-        # payment_id is form_date['payment_id']
-
-        context = { 'payment_id': form_data['payment_id'], 'order_id': form_data['order_id'] }
-
-        template_name = 'confirmation.html'
-        return render(request, template_name, context)
+    
