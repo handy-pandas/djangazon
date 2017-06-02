f67a7c50a99661e5d88cf6aefb397a6aed1d1731
Adam Myers <ANMyers53@yahoo.com>
2017-06-02 11:38:53 -0500

    Added validators for profile form to prevent invalid phone numbers and zip code entries

--- website/forms/forms.py
+++ website/forms/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from website.models.models import Profile
+from website.models.validators.profile_validators import validate_phone, validate_zip
 from django import forms
 
 class UserForm(forms.ModelForm):
@@ -16,7 +17,9 @@ class EditUserForm(forms.ModelForm):
         fields = ('first_name', 'last_name')
 
 class ProfileForm(forms.ModelForm):
+    phone_number = forms.IntegerField(required=False, widget=forms.NumberInput(), validators=[validate_phone])
+    zipcode = forms.IntegerField(required=False, widget=forms.NumberInput(), validators=[validate_zip])
 
     class Meta:
         model = Profile
-        fields = ('phone_number', 'address', 'city', 'state', 'zipcode')
+        fields = ('address', 'city', 'state', 'zipcode', 'phone_number')
--- website/views/edit_account_view.py
+++ website/views/edit_account_view.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.template import RequestContext
-from website.models.models import Profile
+from website.models.models import Profile, User
 from website.forms.forms import ProfileForm, EditUserForm, UserForm
 
 def edit_account(request):
@@ -22,33 +22,40 @@ def edit_account(request):
         Adam Myers
     """
     if request.method == "POST":
+        user = User.objects.get(id=request.user.id)
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.save()
 
-        form_data = request.POST
-
-        user = request.user
-        user_form = UserForm(request.POST or None, instance=user)
-        if user_form.is_valid():
-            user_form.save()
-
-        profile = Profile.objects.get(pk=user.id)
+        profile = Profile.objects.get(pk=request.user.id)
         profile_form = ProfileForm(request.POST or None, instance=profile)
+
         if profile_form.is_valid():
             profile_form.save()
+        else:
+            return show_edit_account(request, error=profile_form.errors.values())
 
-        context = {'profile': profile}
+        context = {'profile': profile, 'user': user}
         template_name = 'profile.html'
         return render(request, template_name, context)
     else:
-        u = request.user
-        user_form = EditUserForm(initial={'first_name': u.first_name, 'last_name': u.last_name})
+        return show_edit_account(request)
 
-        u_p = Profile.objects.get(pk=u.id)
-        profile_form = ProfileForm(initial={'address': u_p.address, 'phone_number': u_p.phone_number, 'city': u_p.city, 'state': u_p.state, 'zipcode': u_p.zipcode})
+def show_edit_account(request, error=False):
+    u = request.user
+    user_form = EditUserForm(initial={'first_name': u.first_name, 'last_name': u.last_name})
 
+    u_p = Profile.objects.get(pk=u.id)
+    profile_form = ProfileForm(initial={'address': u_p.address, 'phone_number': u_p.phone_number, 'city': u_p.city, 'state': u_p.state, 'zipcode': u_p.zipcode})
+
+    if error == False:
         context = {'profile_form': profile_form, 'user_form': user_form}
+    else:
+        context = {'profile_form': profile_form, 'user_form': user_form, 'message': error}
+        
 
-        template_name = 'edit_account.html'
-        return render(request, template_name, context)
+    template_name = 'edit_account.html'
+    return render(request, template_name, context)
 
 
 
